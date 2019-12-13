7d1530a569dbcbd846fecdd4ba6943bb492eaf09
a1844472398 <1844472398@qq.com>
2019-12-13 21:55:54 +0800

    vuh

---
+++ 2019-12-13/helloworld/routes/post.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var router = express.Router();
+
+
+let person = [
+  'tony',
+  'dida',
+  'pjc',
+  'pl',
+  '尹德智'
+];
+
+router.post('/', function(req, res, next) {
+  let obj = {
+    code:0,
+    msg:'有介个银了啦!'
+  }
+  let json = req.body;
+  console.log(req);
+  if(!person.includes(json.user)){
+      obj.code = 1;
+      obj.msg = '木有介个银!';
+  }
+  res.send(JSON.stringify(obj));
+});
+
+module.exports = router;
